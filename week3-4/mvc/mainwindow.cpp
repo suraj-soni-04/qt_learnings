d12477f4d22a6f9af52b010213c47bd9d3f1581d
Suraj Soni <suraj.soni@cctech.co.in>
2024-07-30 16:22:54 +0530

    week-3

--- week3-4/mvc/mainwindow.cpp
+++ week3-4/mvc/mainwindow.cpp
@@ -10,8 +10,19 @@ MainWindow::MainWindow(QWidget *parent)
     listModel = new CustomListModel(this);
     tableModel = new CustomTableModel(this);
 
-    ui->listView->setModel(listModel);
-    ui->tableView->setModel(tableModel);
+    // ui->listView->setModel(listModel);
+    // ui->tableView->setModel(tableModel);
+
+    listProxyModel = new QSortFilterProxyModel(this);
+    listProxyModel->setSourceModel(listModel);
+    listProxyModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+
+    tableProxyModel = new QSortFilterProxyModel(this);
+    tableProxyModel->setSourceModel(tableModel);
+    tableProxyModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+
+    ui->listView->setModel(listProxyModel);
+    ui->tableView->setModel(tableProxyModel);
 
     // Adding initial data
     listModel->addItem("John Doe - Engineer");
@@ -23,6 +34,9 @@ MainWindow::MainWindow(QWidget *parent)
     tableModel->addItem({"Mike Johnson", "45", "Manager"});
 
     connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::on_pushButton_clicked);
+    connect(ui->filterLineEdit, &QLineEdit::textChanged, this, &MainWindow::on_filterLineEdit_textChanged);
+    connect(ui->sortNameButton, &QPushButton::clicked, this, &MainWindow::on_sortNameButton_clicked);
+    connect(ui->sortAgeButton, &QPushButton::clicked, this, &MainWindow::on_sortAgeButton_clicked);
 }
 
 MainWindow::~MainWindow()
@@ -35,3 +49,20 @@ void MainWindow::on_pushButton_clicked()
     listModel->addItem("New Person - Occupation");
     tableModel->addItem({"New Person", "0", "Occupation"});
 }
+
+void MainWindow::on_filterLineEdit_textChanged(const QString &text)
+{
+    listProxyModel->setFilterWildcard(text);
+    tableProxyModel->setFilterWildcard(text);
+}
+
+void MainWindow::on_sortNameButton_clicked()
+{
+    tableProxyModel->sort(0, Qt::AscendingOrder); // Sort by first column (Name)
+}
+
+void MainWindow::on_sortAgeButton_clicked()
+{
+    tableProxyModel->sort(1, Qt::AscendingOrder); // Sort by second column (Age)
+}
+
